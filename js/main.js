3819f676ae0c8a6c6e2a283090bf2fbe22ee9284
carolinalqzt@gmail.com <carolinalqzt@gmail.com>
2020-08-05 00:14:40 -0300

    Hall y Lisa's Room subidos al master

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ var letter = "H"
  function show(letter) {
     document.getElementById("letter").innerHTML = letter
 }
+(function(){
+  var saludo = function(){
+    
+      boton.innerHTML= "<img class='letraa' src='https://i.pinimg.com/originals/99/ee/50/99ee50dbb4c1b790e277acd66f511bbc.gif'></img><img class='lisafeliz' src='http://www.mundosimpson.com.ar/galerias/anim/xlisa02.gif'> "
+   };
+   let boton = document.getElementById("boton")
+    boton.addEventListener("click", saludo);
+  }());
+  (function(){
+    
+     let puerta = document.getElementById("puertacentral")
+      puerta.addEventListener("click",function(){puerta.style.transform = "rotateY(110deg)"} );
+    }())
