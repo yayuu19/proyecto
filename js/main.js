1fdb0829c2e9cfcd3ecfcce93c4101ce147079d6
aylencandela <aylencandela@hotmail.com>
2020-08-04 22:25:01 -0300

    Cambiando la intro y otros detalles

--- js/main.js
+++ js/main.js
@@ -1,31 +1,34 @@
 //location.href='https://yayuu19.github.io/startavina-desafio1/index#intro';
 
-let diningRoom = document.getElementById("dining-room");
-let switcher = document.getElementById("switcher");
-let darkness = document.getElementById("darkness");
-let span = document.getElementById("close");
-let hint = document.getElementById("hint");
+let start = document.getElementById("start");
+let homerRunning = document.getElementById("homer-running");
 
-switcher.addEventListener("click", function(){
-    switcher.style.display = "none";
-    darkness.className = "off";
-    span.style.display = "block";
-    hint.innerText = "H";
-    hint.style.color = "yellow";
-    hint.style.opacity = "1";
+start.addEventListener("click", function(){
+    homerRunning.style.animation = "run 2s ease forwards";
+})
+
+let switcherOn = document.getElementById("switcher-on");
+let switcherOff = document.getElementById("switcher-off");
+let darkness = document.getElementById("darkness");
+let hint = document.getElementById("hint");
+let glass = document.getElementById("glass");
+let curtain = document.getElementById("curtain");
+
+switcherOn.addEventListener("click", function(){
+    switcherOn.style.display = "none";
+    switcherOff.style.display = "block";
+    darkness.className = "lights-off";
+    hint.innerText = "A";
+    hint.style.zIndex = "5";
  });
 
-span.addEventListener("click", function(){
-    switcher.style.display = "block";
-    span.style.display = "none";
-    span.parentNode.className = "";
-    hint.innerText = "A";
-    hint.style.color = "skyblue";
-    hint.style.opacity = "0.8";
+switcherOff.addEventListener("click", function(){
+    switcherOn.style.display = "block";
+    switcherOff.style.display = "none";
+    darkness.className = "";
+    hint.innerText = "H";
+    hint.style.zIndex = "1";
 });
-
-
-let curtain = document.getElementById("curtain");
 
 curtain.addEventListener("click", function(){
     curtain.style.transform = "translateX(90px)";
@@ -33,13 +36,10 @@ curtain.addEventListener("click", function(){
     curtain.style.transitionTimingFunction = "ease-in-out";
 })
 
-let glass = document.getElementById("glass");
-
 glass.addEventListener("click", function(){
     glass.style.transformOrigin = "bottom";
     glass.style.transform = "rotateZ(90deg) translate(150px)";
     glass.style.transitionTimingFunction = "ease-in";
     glass.style.transitionDuration = "2s";
+    glass.style.transitionDelay = "3s";
 })
-
-
